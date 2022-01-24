d24ead2a8e0a4d32516a1e35bb1fbceb98969e4a
Ryo-1157 <75074471+Ryo-1157@users.noreply.github.com>
2022-01-25 02:13:53 +0900

    create organism

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organism/user/UserCard";
 import "./styles.css";
+
+const user = {
+  image:
+    "https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80",
+  name: "Guy",
+  email: "123456789@test.com",
+  phone: "123-456-7890",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "http://test.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +23,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput></SearchInput>
+      <UserCard user={user} />
     </div>
   );
 }
